0a3d1e16cac8f1383bae8db5a4d349755e5e17de
Naufaldi Rafif S <naufaldi.rafif@efishery.com>
2023-02-08 09:03:34 +0700

    feat(animation): fade in

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,16 +2,41 @@ import { Box, Container, Flex } from '@chakra-ui/react';
 import React, { ReactNode } from 'react';
 import Footer from './footer';
 import Header from './header';
+import { motion } from 'framer-motion';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
+const variants = {
+  hidden: {
+    opacity: 0,
+    y: 100,
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1, ease: 'easeInOut' },
+  },
+};
+
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <Flex minH={'100vh'} flexDir="column">
       <Header />
-      <Container maxW={'6xl'}>{children}</Container>
+      <Container
+        as={motion.div}
+        initial="hidden"
+        animate="visible"
+        variants={variants}
+        maxW={'6xl'}
+      >
+        {children}
+      </Container>
       <Footer />
     </Flex>
   );
